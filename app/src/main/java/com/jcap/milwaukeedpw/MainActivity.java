432994e4d653683422db7f54777327017816c3aa
Jason Capriotti <jason.capriotti@gmail.com>
2017-01-24 07:53:22 -0600

    Another partial update

--- app/src/main/java/com/jcap/milwaukeedpw/MainActivity.java
+++ app/src/main/java/com/jcap/milwaukeedpw/MainActivity.java
@@ -1,20 +1,27 @@
 package com.jcap.milwaukeedpw;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.jcap.milwaukeedpw.utility.VersionHelper;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -48,9 +55,31 @@ public class MainActivity extends AppCompatActivity
         sharedPref.registerOnSharedPreferenceChangeListener(this);
 
         loadDisplay(sharedPref);
+
+        checkFirstRun();
     }
 
-    
+    public void checkFirstRun() {
+        String version = VersionHelper.getVersion(this);
+        String preferenceKey = String.format("isFirstRun_%s", version);
+        boolean isFirstRun = true;//getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean(preferenceKey, true);
+        if (isFirstRun) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.new_version_title)
+                    .setMessage(R.string.new_version_details)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_info)
+                    .show();
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                    .edit()
+                    .putBoolean(preferenceKey, false)
+                    .apply();
+        }
+    }
 
     private void loadDisplay(SharedPreferences prefs) {
         TextView nextPickupText = (TextView)findViewById(R.id.nextPickupText);
@@ -124,13 +153,41 @@ public class MainActivity extends AppCompatActivity
         }
         else if (id == R.id.nav_alerts) {
             Intent intent = new Intent();
-            intent.setClassName(this, "com.jcap.milwaukeedpw.RequestServiceActivity");
+            intent.setClassName(this, "com.jcap.milwaukeedpw.AlertActivity");
             startActivity(intent);
         }
+        else if (id == R.id.nav_review) {
+            openAppStore();
+        }
+
+        item.setChecked(false);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void openAppStore() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        }
+        else{
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        }
+
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+        }
     }
 
 }
