5fdfd39f1a2ba25386be29a337ed50113e326cea
Jason Capriotti <jcapriotti@directs.com>
2017-02-06 10:27:45 -0600

    Update SDK version and screenshots

--- app/src/main/java/com/jcap/milwaukeedpw/MainActivity.java
+++ app/src/main/java/com/jcap/milwaukeedpw/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity
 
         loadDisplay(sharedPref);
 
-        checkFirstRun();
+//        checkFirstRun();
         resetClickCounter = 0;
     }
 
